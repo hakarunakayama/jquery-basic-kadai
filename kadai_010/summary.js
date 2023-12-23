000122cd4ad34494c3251755c3a64b336feeb6f7
hakarunakayama <151834966+hakarunakayama@users.noreply.github.com>
2023-12-23 22:42:25 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,17 @@
+$('#change-color').on('click', function(){
+  $('#target').css('color', 'red');
+});
+
+$('#change-text').on('click', function(){
+  $('#target').text('文字内容を変更しました！');
+});
+
+$('#fade-out').on('click', function(){
+  $('#target').fadeOut();
+});
+
+$('#fade-in').on('click', function(){
+  $('#target').fadeIn();
+});
+
+
